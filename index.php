25fa0aa8aaece90405402b4627f39de729a12c28
Jean Max Guezzo <jean.max.guezzo@gmail.com>
2024-10-08 08:22:22 -0300

    TEP
    TEP

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php
-
-// Seu código aqui
-echo "Hello, World!";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Api Ensino medio</title>
+</head>
+<body>
+  <h1>Hello World!</h1>
+  <p>Este é um exemplo de uma API RESTful com PHP.</p>
+</body>
+</html>
